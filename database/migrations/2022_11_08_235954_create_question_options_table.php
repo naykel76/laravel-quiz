1fa3770dac0e6b4af1727d1cd15e6c8ab0566dcc
Nathan <nathan@naykel.com.au>
2022-11-09 10:27:38 +1000

    initial setup with migrations and samples

---
+++ database/migrations/2022_11_08_235954_create_question_options_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_options', function (Blueprint $table) {
+            $table->id();
+            $table->string('option_text');
+            $table->integer('is_correct')->nullable()->default(0);
+            $table->integer('sort_order')->nullable()->default(0);
+            $table->foreignId('question_id')->references('id')->on('questions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_options');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $quiz1 = \App\Models\Quiz::create(['title' => 'Quiz One']);
+        $quiz2 = \App\Models\Quiz::create(['title' => 'Quiz Two']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $quiz1->questions()->create(['id' => 72, 'question' => 'What is the colour of the ocean?'])
+            ->options()->createMany([
+                ['option_text' => 'Blue', 'is_correct' => 1],
+                ['option_text' => 'Yellow'],
+                ['option_text' => 'Pink'],
+                ['option_text' => 'Green'],
+            ]);
+
+        $quiz1->questions()->create(['question' => 'Is fire hot?'])
+            ->options()->createMany([
+                ['option_text' => 'Yes', 'is_correct' => 1],
+                ['option_text' => 'No'],
+            ]);
+
+        $quiz1->questions()->create(['question' => 'How many wheels does a car have?'])
+            ->options()->createMany([
+                ['option_text' => '4', 'is_correct' => 1],
+                ['option_text' => '128'],
+                ['option_text' => '16'],
+                ['option_text' => '2'],
+            ]);
+
+        $quiz1->questions()->create(['question' => '247 is greater than 987'])
+            ->options()->createMany([
+                ['option_text' => 'True'],
+                ['option_text' => 'False', 'is_correct' => 1],
+            ]);
+
+        $quiz2->questions()->create(['id' => 427, 'question' => 'What color is the sun?'])
+            ->options()->createMany([
+                ['id' => 80, 'option_text' => 'Blue'],
+                ['option_text' => 'Yellow', 'is_correct' => 1],
+                ['option_text' => 'Red'],
+            ]);
+
+        $quiz2->questions()->create(['question' => 'Capital of France?'])
+            ->options()->createMany([
+                ['option_text' => 'Toulouse'],
+                ['option_text' => 'Paris', 'is_correct' => 1],
+                ['option_text' => 'Lyon'],
+            ]);
+
+        $quiz2->questions()->create(['question' => 'NBA: A stands for...?'])
+            ->options()->createMany([
+                ['option_text' => 'Association', 'is_correct' => 1],
+                ['option_text' => 'America'],
+                ['option_text' => 'Act'],
+            ]);
     }
 }
